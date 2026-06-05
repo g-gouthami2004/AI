9204b2c5c5be879365a8099d7260ed8edbb0c40c
gouthami <gouthamirao1734@gmail.com>
2026-06-05 13:20:18 +0530

    Refactor PDF processing and agent interaction
    Updated PDF loading and processing logic, adjusted chunk size for text splitting, and modified agent prompt. Changed the PDF file name and improved context retrieval functionality.

--- main.py
+++ main.py
@@ -1,48 +1,60 @@
 import os
-from groq import Groq
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.vectorstores import InMemoryVectorStore
-from langchain_core.tools import tool
-from langchain.agents import create_agent
+from langchain_core.tools import tool 
+from langchain.agents import create_agent 
 
 load_dotenv()
 
 model = ChatGroq(model="qwen/qwen3-32b", reasoning_format="parsed")
-print("Loading PDF Doc...")
-loader = PyPDFLoader('AGL_V_BonfireBlaze.pdf')
+
+print("Loading the PDF document..")
+loader = PyPDFLoader("AGL_V_WesternBlaze.pdf")
 docs = loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
-all_splits  = text_splitter.split_documents(docs)
- 
-print("Generating embeddings....")
-embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
-vector_store = InMemoryVectorStore.from_documents(all_splits,embeddings)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+all_splits = text_splitter.split_documents(docs)
+
+print("Building embeddings...")
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+vector_store = InMemoryVectorStore.from_documents(all_splits, embeddings)
 
 @tool
-def retrieve_context(query : str)->str:
-    """Retrieves relevant context from the PDf document based on the query."""
-    similar_docs = vector_store.similarity_search(query,k=5 )
+def retrieve_context(query: str)->str:
+    """Retrieves relevant context from the PDF document based on the query."""
+    similar_docs = vector_store.similarity_search(query, k=3)
     data = []
     for doc in similar_docs:
         content = doc.page_content
-        source = doc.metadata.get("source","unknown")
+        source = doc.metadata.get("source", "unknown")
         data.append(f"Content: {content}\nSource: {source}")
-    return "\n".join(data)
+    return "\n\n".join(data)
 
 tools = [retrieve_context]
-prompt = "You are an agent who retrieves data from PDF."
-agent = create_agent(model, [retrieve_context],system_prompt=prompt)
-query = "what are datatypes and give the previous response too?"
+client = "You are an agent who retrieves context from PDF docs."
+agent = create_agent(model, tools, system_prompt=client)
 
-for step in agent.stream({"messages": [
-    {
-        'role':'user',
-        'content':query
-    }
-]}, stream_mode="values"):
-    step["messages"][-1].pretty_print()
+chat_history = []
+
+print("Welcome to RAG chatbot.Enter your query below.")
+while True:
+    query=input("\nYou : ").strip()
+    if query.lower() in ["exit", "quit", "bye","goodbye"]:
+        print("AI : Good Bye!")
+        break
+    if not query:
+        continue
+
+    chat_history.append({"role": "user", "content": query})
+    
+    response = agent.invoke({"messages": chat_history})
+
+    ai_message = response["messages"][-1]
+    ai_message.pretty_print()
+
+    chat_history.append({"role": "assistant", "content": ai_message.content}) 
+  
